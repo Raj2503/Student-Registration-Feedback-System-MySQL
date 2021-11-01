faa666bc1fff29bd9f2ad0002e9a4aadb87ce5de
Raj Aryan <raj250301@gmail.com>
2021-11-01 23:56:07 +0530

    tried

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+
+// var sql = 'SELECT * FROM registeration';
+
 const mysql = require("mysql");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 var app = express();
 
-app.use(bodyParser.json());
+app.use(json());
 
-var mysqlConnection = mysql.createConnection({
+var mysqlConnection = createConnection({
     host: "localhost",
     user: "root",
     password: "oracle",
@@ -21,3 +25,25 @@ mysqlConnection.connect((err) => {
         console.log("DB connection failed \n Error : " + JSON.stringify(err, undefined, 2));
 });
 app.listen(3000);
+function myfunction(){
+   
+
+
+
+
+
+    
+    console.log("Function Called");
+    var sql = "INSERT INTO REGISTERATION VALUES ('Sarthak ARYAN','M','O+','raj250301@gmail.com',95175371,222222)";
+    mysqlConnection.query(sql, function(err,result,fields){
+    if(err) throw err;
+    console.log("1 row inserted");
+});
+}
+
+myfunction();
+
+
+function func1(){
+    console.log("raj");
+}
